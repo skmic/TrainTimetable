c24c7b847952e67c00640614cb18d3877d5f5b9b
Sahil Kumar <sahilrk911@gmail.com>
2017-01-15 21:26:52 +0100

    Basic functionality added, displaying train intervals for every stop

--- TrainTimetable.cpp
+++ TrainTimetable.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <sstream>
+#include <chrono>
+#include <ctime>
 using namespace std;
+using namespace std::chrono;
 
 class TrainTimetable
 {
@@ -28,7 +31,24 @@ public:
 //Class functions
 void TrainTimetable::vDisplayTimetable(void)
 {
-  std::cout<<"Displaying train time table below" <<endl;
+  string Temp;
+  std::stringstream TempStream;
+  cout<<"Displaying train time table below" <<endl;
+  cout << "Linie " << this->m_sName << " "; 
+  if(this->m_bDirection)
+    cout << "North" << endl;
+  else
+    cout << "South" << endl;
+  for (std::size_t i = 0; i != this->m_Stops.size(); ++i)
+  {
+    TempStream.str("");
+    TempStream << this->m_Stops[i];
+    TempStream << "    " << this->m_StopTimes[i] << "    " << this->m_StopTimes[i] + this->m_nTrainFrequency << endl;
+
+
+    cout << TempStream.str() << endl;
+    TempStream.clear();
+  }
 }
 
 int main(int argc, char **argv)
@@ -36,30 +56,44 @@ int main(int argc, char **argv)
   string sName;
   string sStop;
   int nStopTime;
-  bool bDirection;
+  bool bDirection = true;
   std::vector<string> Stops;
   std::vector<int> StopTimes;
-  int nTrainFrequency;
-
+  int nTrainFrequency = 15;
+  time_t tt;
+  system_clock::time_point today = system_clock::now();
+  tt = system_clock::to_time_t(today);
 
   std::cout << "Train Time Table Display" <<endl;
+
+  cout << ctime(&tt) <<endl;
+
+
   cout << "Enter train data" << endl;
   cout << "Train Name" << endl;
   cin >> sName;
+ // cout << "Train Frequency" << endl;
+ // cin >> nTrainFrequency;
+
+ // cout << "Train Direction" << endl;
+ // cin >> bDirection;
+  
   cout << "Stops" << endl;
   while (cin >> sStop)
   {
-    if(sStop!="\n")
+    if(sStop!="STOP")
       Stops.push_back(sStop);
     else break;
   }
   cout << "Stop Times in minutes" << endl;
   while (cin >> nStopTime)
-    StopTimes.push_back(nStopTime);
-  cout << "Train Frequency" << endl;
-  cin >> nTrainFrequency;
+  {
+    if(nStopTime != 99)
+      StopTimes.push_back(nStopTime);
+    else break;
+  }
 
-  TrainTimetable t;
+  TrainTimetable t(sName, bDirection, Stops, StopTimes, nTrainFrequency);
   t.vDisplayTimetable();
 
   return 0;
